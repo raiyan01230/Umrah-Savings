aea3f6366a5d5ad03f9c42f5caa8c96658c951b7
raiyan01230 <raiyanclothingelite@gmail.com>
2026-07-13 14:26:49 +0600

    chore: update base path for deployment
    Set base to '/Umrah-Savings/' to ensure correct asset loading on GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Umrah-Savings/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
